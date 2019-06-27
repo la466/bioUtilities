43e003c5ace6baf9a1ddef2535b0b558c739405f
la466 <l.abrahams@bath.ac.uk>
2019-06-27 16:20:56 +0100

    add the ability to retain motif hits as hits per motif

--- bioUtilities/seq/motif_hit_positions.py
+++ bioUtilities/seq/motif_hit_positions.py
@@ -1,7 +1,7 @@
 import numpy as np
 import re
 
-def motif_hit_positions(sequence, motifs):
+def motif_hit_positions(sequence, motifs, flatten = True):
     """
     Return the positions at which a motif in a set of motifs hits a sequence
 
@@ -11,6 +11,9 @@ def motif_hit_positions(sequence, motifs):
         The sequence
     motifs : list
         The list of motifs to query for
+    flatten : bool
+        If set, flatten the hit list so its all hits. If false, this is the
+        list of indices for each motif hit grouped by each hit
 
     Returns
     ---------
@@ -29,6 +32,15 @@ def motif_hit_positions(sequence, motifs):
     # if the given item is a list of sequences, iterate through
     matches = re.finditer(motif_search, sequence)
     motif_hits = []
-    [motif_hits.extend(list(range(hit.span()[0], hit.span()[0] + len(hit.group(1))))) for hit in matches]
-    motif_hits = sorted(list(set(motif_hits)))
+    # for each hit, add the indices for each position of the hit as a list to
+    # the global list
+    for hit in matches:
+        motif_hits.append(list(range(hit.span()[0], hit.span()[0] + len(hit.group(1)))))
+    # now flatten if we want just a list of all positions hit
+    if flatten:
+        temp_hits = []
+        [temp_hits.extend(i) for i in motif_hits]
+        motif_hits = temp_hits
+        motif_hits = sorted(list(set(motif_hits)))
+
     return motif_hits
