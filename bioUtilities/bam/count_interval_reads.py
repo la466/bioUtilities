6931854669c46f2c8d306e4f33969029063a7978
la466 <l.abrahams@bath.ac.uk>
2019-07-10 17:34:40 +0100

    force indexing on intersect

--- bioUtilities/bam/count_interval_reads.py
+++ bioUtilities/bam/count_interval_reads.py
@@ -2,7 +2,7 @@ import os
 
 from bioUtilities.commands import run_process
 
-def count_interval_reads(input_bed, input_bam, output_file, force_strand = False):
+def count_interval_reads(input_bed, input_bam, output_file, force_strand = False, index_file = True):
     """
     For each interval in bed format, count the number of reads in the bam file
 
@@ -14,6 +14,10 @@ def count_interval_reads(input_bed, input_bam, output_file, force_strand = False
         Path to the .bam file containing the reads
     output_file : str
         Path to the output file
+    force_strand : bool
+        If set, force reads to match strand in bed file
+    index_file : bool
+        If set, create the index file
 
     Examples
     ---------
@@ -22,7 +26,7 @@ def count_interval_reads(input_bed, input_bam, output_file, force_strand = False
     """
 
     # need to index the .bam file if it doesnt already exist
-    if not os.path.exists("{0}.bai".format(input_bam)):
+    if not os.path.exists("{0}.bai".format(input_bam)) or not index_file:
         index_args = ["samtools", "index", input_bam]
         run_process(index_args)
 
