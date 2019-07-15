66d122d375be62eaa8c0182cf516b86af5b391f9
Liam Abrahams <l.abrahams@bath.ac.uk>
2019-07-15 12:12:04 +0100

    update count interval reads to take a bed file and return bed file

--- bioUtilities/bam/count_interval_reads.py
+++ bioUtilities/bam/count_interval_reads.py
@@ -1,23 +1,26 @@
 import os
 
+from bioUtilities.bed import bed_to_saf
 from bioUtilities.commands import run_process
+from bioUtilities.files import remove_file, read_many_fields
 
-def count_interval_reads(input_bed, input_bam, output_file, force_strand = False, index_file = True):
+def count_interval_reads(input_file, input_bam, output_file):
     """
     For each interval in bed format, count the number of reads in the bam file
 
     Parameters
     ---------
-    input_bed : str
-        Path to the .bed file containing the intervals
+    input_file : str
+        Path to the file containing the intervals
     input_bam : str
         Path to the .bam file containing the reads
     output_file : str
         Path to the output file
-    force_strand : bool
-        If set, force reads to match strand in bed file
-    index_file : bool
-        If set, create the index file
+
+
+    Dependencies
+    ---------
+    featureCounts v1.6.4
 
     Examples
     ---------
@@ -25,13 +28,32 @@ def count_interval_reads(input_bed, input_bam, output_file, force_strand = False
     >>> count_interval_reads("exon_junctions.bed", "reads.bam", "exon_junction_reads.bed")
     """
 
-    # need to index the .bam file if it doesnt already exist
-    if not os.path.exists("{0}.bai".format(input_bam)) or not index_file:
-        index_args = ["samtools", "index", input_bam]
-        run_process(index_args)
+    # need to convert to .saf format if in bed format
+    # .saf format its 1-based
+    if input_file[-4:] == ".bed":
+        old_input_file = input_file
+        input_file = "{0}.saf".format(input_file[:-4])
+        bed_to_saf(old_input_file, input_file)
 
-    # now use bedtools to count the reads
-    args = ["bedtools", "multicov", "-s", "-D", "-F", "-bams", input_bam, "-bed", input_bed]
-    if not force_strand:
-        del args[2]
-    run_process(args, file_for_output = output_file)
+    if output_file[-4:] == ".bed":
+        temp_output = "{0}.saf".format(output_file[:-4])
+    else:
+        temp_output = output_file
+
+    # now can use featureCounts to count reads
+    # this return the file in 'saf' format
+    args = ["featureCounts", "-fpO", "-F", "SAF", "-g", "ID", "-a", input_file, "-o", temp_output, input_bam]
+    run_process(args)
+
+    if output_file[-4:] == ".bed":
+        entries = read_many_fields(temp_output)[2:]
+        with open(output_file, "w") as outfile:
+            for entry in entries:
+                output = [entry[1], str(int(entry[2])-1), str(int(entry[3])-1), entry[0], ".", entry[4]]
+                output.extend(entry[5:])
+                outfile.write("{0}\n".format("\t".join(output)))
+
+        remove_file(temp_output)
+
+    if old_input_file[-4:] == ".bed":
+        remove_file(input_file)
